8c139f3b598e686a96c6519f64e15ccf24f12ea1
Tim McNamara <code@timmcnamara.co.nz>
2012-02-28 19:33:21 +1300

    RUST now under AGPL

--- rust.py
+++ rust.py
@@ -1,3 +1,23 @@
+# RUST: corroding misbehaving web bots
+
+# Copyright 2011, 2012 Tim McNamara
+
+# This file is part of RUST.
+
+# RUST is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# RUST is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from gevent import monkey; monkey.patch_all()
 
 from random import SystemRandom, choice, sample, randint, random
